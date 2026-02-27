f5fe1f31a81bd3792e084fc4b412878c6e9edcfb
dushyanthb702 <dushyanthb702@gmail.com>
2026-02-28 00:09:40 +0530

    Delete NeuroIQPro directory

--- NeuroIQPro/static/app.js
+++
@@ -1,9 +0,0 @@
-function toggleTheme() {
-    const body = document.body;
-
-    if (body.classList.contains("light-mode")) {
-        body.classList.remove("light-mode");
-    } else {
-        body.classList.add("light-mode");
-    }
-}
