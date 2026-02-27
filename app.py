f88a5dcd93f1a1d02abf096f17fbe6a821dbf579
dushyanthb702 <dushyanthb702@gmail.com>
2026-02-28 00:19:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -156,5 +156,9 @@ def reset():
     save_leaderboard([])
     return redirect(url_for("leaderboard"))
 
+import os
+
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
